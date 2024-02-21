4aa6a4ef82411a6af964065d2e794a755f9edd1f
Rafael <68301720+Flavoredone@users.noreply.github.com>
2024-02-21 16:56:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -39,102 +39,65 @@ class AppTile(ft.Container):
 
 
 def main(page: ft.Page):
-
-    if str(page.route) == '/privacy_policy':
-        page.add(
-            ft.Row(
-                controls=[
-                    ft.Text("""
-                                   Политика Конфиденциальности: 
-
-                                   Контактный email: info@maksiz.ru
-
-                                   Дата вступления в силу: 23 декабря 2023 года.
-                                   
-                                   Приложение «MAKSIZ» является оффлайн-приложением и не 
-                                   собирает, не хранит и не передает персональные данные 
-                                   пользователя. 
-                                   
-                                   Приложение не требует доступа к интернету 
-                                   или другим функциям устройства пользователя для своей 
-                                   работы, кроме функций, необходимых для основного 
-                                   функционала (например, просмотр текстов песен). 
-                                   
-                                   Пользователи могут использовать приложение для просмотра 
-                                   текстов песен, без необходимости предоставлять какую-либо 
-                                   персональную информацию. 
-                                   
-                                   В случае вопросов или предложений, связанных с работой
-                                   приложения или политикой конфиденциальности, пожалуйста,
-                                   свяжитесь с нами по адресу: info@maksiz.ru
-                                   """,
-                            height=int(page.height),
-                            selectable=True)
-                ],
-                alignment=ft.MainAxisAlignment.CENTER,
-            )
+    page.add(
+        ft.Column(
+            tight=True,
+            scroll=ft.ScrollMode.AUTO,
+            controls=[
+                ft.Text(height=1),
+                AppTile(
+                    name="О КОМПАНИИ",
+                    file_name="company.py",
+                    view=company.example(page),
+                    content=ft.Image(src="1.png",
+                                     # width=250,
+                                     height=int(int(page.height) / 5.5),
+                                     fit=ft.ImageFit.FIT_HEIGHT
+                                     ),
+                ),
+                AppTile(
+                    name="ПОИСК ПО НОМЕРУ CAS",
+                    file_name="search.py",
+                    view=search.example(page),
+                    content=ft.Image(src="2.png",
+                                     # width=250,
+                                     height=int(int(page.height) / 5.5),
+                                     fit=ft.ImageFit.FIT_HEIGHT
+                                     ),
+                ),
+                AppTile(
+                    name="КАТАЛОГ",
+                    file_name="catalogue.py",
+                    view=catalogue.example(page),
+                    content=ft.Image(src="3.png",
+                                     # width=250,
+                                     height=int(int(page.height) / 5.5),
+                                     fit=ft.ImageFit.FIT_HEIGHT
+                                     ),
+                ),
+                AppTile(
+                    name="ДОКУМЕНТАЦИЯ",
+                    file_name="certif.py",
+                    view=certif.example(page),
+                    content=ft.Image(src="4.png",
+                                     # width=250,
+                                     height=int(int(page.height) / 5.5),
+                                     fit=ft.ImageFit.FIT_HEIGHT
+                                     ),
+                ),
+                AppTile(
+                    name="ГДЕ КУПИТЬ",
+                    file_name="where.py",
+                    view=where.example(page),
+                    content=ft.Image(src="5.png",
+                                     # width=250,
+                                     height=int(int(page.height) / 5.5),
+                                     fit=ft.ImageFit.FIT_HEIGHT
+                                     ),
+                ),
+            ],
         )
-
-    else:
-        page.add(
-            ft.Column(
-                tight=True,
-                scroll=ft.ScrollMode.AUTO,
-                controls=[
-                    ft.Text(height=1),
-                    AppTile(
-                        name="О КОМПАНИИ",
-                        file_name="company.py",
-                        view=company.example(page),
-                        content=ft.Image(src="1.png",
-                                         # width=250,
-                                         height=int(int(page.height) / 5.5),
-                                         fit=ft.ImageFit.FIT_HEIGHT
-                                         ),
-                    ),
-                    AppTile(
-                        name="ПОИСК ПО НОМЕРУ CAS",
-                        file_name="search.py",
-                        view=search.example(page),
-                        content=ft.Image(src="2.png",
-                                         # width=250,
-                                         height=int(int(page.height) / 5.5),
-                                         fit=ft.ImageFit.FIT_HEIGHT
-                                         ),
-                    ),
-                    AppTile(
-                        name="КАТАЛОГ",
-                        file_name="catalogue.py",
-                        view=catalogue.example(page),
-                        content=ft.Image(src="3.png",
-                                         # width=250,
-                                         height=int(int(page.height) / 5.5),
-                                         fit=ft.ImageFit.FIT_HEIGHT
-                                         ),
-                    ),
-                    AppTile(
-                        name="ДОКУМЕНТАЦИЯ",
-                        file_name="certif.py",
-                        view=certif.example(page),
-                        content=ft.Image(src="4.png",
-                                         # width=250,
-                                         height=int(int(page.height) / 5.5),
-                                         fit=ft.ImageFit.FIT_HEIGHT
-                                         ),
-                    ),
-                    AppTile(
-                        name="ГДЕ КУПИТЬ",
-                        file_name="where.py",
-                        view=where.example(page),
-                        content=ft.Image(src="5.png",
-                                         # width=250,
-                                         height=int(int(page.height) / 5.5),
-                                         fit=ft.ImageFit.FIT_HEIGHT
-                                         ),
-                    ),
-                ],
-            )
-        )
+    )
 
     def view_pop(view):
         page.views.pop()
